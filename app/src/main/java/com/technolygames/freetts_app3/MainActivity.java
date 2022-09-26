a6c6c9839b56820b1d8760b93c13bada7eb1e8ed
TechnolyGames <technolygames@gmail.com>
2022-09-25 20:15:45 -0500

    TTS added and tested

--- app/src/main/java/com/technolygames/freetts_app3/MainActivity.java
+++ app/src/main/java/com/technolygames/freetts_app3/MainActivity.java
@@ -11,21 +11,31 @@ import javax.speech.AudioException;
 import javax.speech.EngineException;
 //android
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.navigation.Navigation;
 import androidx.navigation.NavController;
 //extensión larga
 import androidx.navigation.ui.NavigationUI;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.core.app.NotificationCompat;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
 
-public class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+public class MainActivity extends androidx.appcompat.app.AppCompatActivity{
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    private TextToSpeech tts;
+
+    private String CHANNEL_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -40,10 +50,34 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity {
         appBarConfiguration=new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
 
+        tts=new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int i){
+                if(i!=TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                TextView ta=(TextView)findViewById(R.id.edit_text);
+                Toast.makeText(getApplicationContext(), ta.getText().toString(), Toast.LENGTH_SHORT).show();
+                tts.setLanguage(new Locale("es","mx"));
+                tts.speak(ta.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
         binding.fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Snackbar.make(view,"Replace with your own action",Snackbar.LENGTH_LONG).setAction("Action",null).show();
+                Snackbar.make(view,"Lee el texto del área de texto",Snackbar.LENGTH_LONG).setAction("Acción",new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        //NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID).setSmallIcon(R.drawable.ic_launcher_background).setContentTitle("Test").setContentText("Content text").setStyle(new NotificationCompat.BigTextStyle().bigText("big style text")).setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    }
+                }).show();
             }
         });
     }
@@ -76,22 +110,11 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity {
         return NavigationUI.navigateUp(navController,appBarConfiguration)||super.onSupportNavigateUp();
     }
 
-    protected void synthesizer(){
-        try {
-            System.setProperty("freetts.voices","com.sun.speech.freetts.en.us"+".cmu_us_kal.KevinVoiceDirectory");
-            Central.registerEngineCentral("com.sun.speech.freetts"+".jsapi.FreeTTSEngineCentral");
-            Synthesizer synth=Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
-            synth.allocate();
-            synth.resume();
-            synth.speakPlainText("", null);
-            synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
-            synth.deallocate();
-        }catch(AudioException e){
-            e.printStackTrace();
-        }catch(EngineException x){
-            x.printStackTrace();
-        }catch(InterruptedException n){
-            n.printStackTrace();
+    protected void onPause(){
+        if(tts!=null){
+            tts.stop();
+            tts.shutdown();
         }
+        super.onPause();
     }
 }
